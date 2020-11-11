252c150bed0bfb91762572d5ab402c67780006c5
시혁 박 <exon.indiv@gmail.com>
2020-11-11 21:36:04 +0900

    Avatar

--- app/src/data/GetUser.ts
+++ app/src/data/GetUser.ts
@@ -1,10 +1,12 @@
-import GetStars from './GetStars'
-import GetFollowers from './GetFollowers'
+import getStars from './GetStars'
+import getFollowers from './GetFollowers'
+import getAvatarUrl from './GetAvatarUrl'
 import User from './User'
 
 export default async function getUser(name: string) {
-    const stars = await GetStars(name)
-    const followers = await GetFollowers(name)
-    const user: User = new User(name, stars, followers)
+    const stars = await getStars(name)
+    const followers = await getFollowers(name)
+    const avatarUrl = await getAvatarUrl(name)
+    const user: User = new User(name, stars, followers, avatarUrl)
     return user
 }
--- app/src/data/User.ts
+++ app/src/data/User.ts
@@ -2,11 +2,13 @@ class User {
     name: string
     stars: number
     followers: number
+    avatarUrl: string
 
-    constructor (name: string, stars: number, followers: number) {
+    constructor (name: string, stars: number, followers: number, avatarUrl: string) {
         this.name = name
         this.stars = stars
         this.followers = followers
+        this.avatarUrl = avatarUrl
     }
 }
 
