5f45779313ff2e8599eab3f6d7cfa2b93d6cf68d
시혁 박 <exon.indiv@gmail.com>
2020-11-13 21:49:12 +0900

    props

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import React from 'react'
 import './css/App.css'
 import './components/UserContent'
 import UserContent from './components/UserContent'
+import getUser from './data/GetUser'
+import User from './data/User'
 
 function App() {
     return (
         <div className="App">
             <header>
-                <h1>Team WAVE Member Star Ranking</h1>
+                <h1 className="title">Team WAVE Member Star Ranking</h1>
             </header>
             <div className="users">
-                <UserContent />
+                <UserContent name="1-exon" stars={10} followers={100} avatarUrl={"https://avatars2.githubusercontent.com/u/56220973?s=460&u=f0786c8d37a31c92e7885e31ebfbc153c731271a&v=4"}/>
             </div>
         </div>
     )
--- app/src/components/UserContent.tsx
+++ app/src/components/UserContent.tsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 import getUser from '../data/GetUser'
 
-class UserContent extends Component {
-    public render() {
-        console.log(getUser('1-exon'))
-        return (
-            <div className="user">
-                <img src="https://avatars3.githubusercontent.com/u/56220973?v=4" className="user-profile" />
-                <h1 id="name" className="user-name">1-EXON</h1>
-                <p id="stars" className="user-content">100</p>
-                <p id="followers" className="user-content">100</p>
-            </div>
-        )
-    }
+type Props = {
+    name: string,
+    stars: number,
+    followers: number,
+    avatarUrl: string
+}
+
+function UserContent({ name, stars, followers, avatarUrl }: Props) {
+    return (
+        <div className="user">
+            <img src={avatarUrl} className="user-profile" />
+            <h1 id="name" className="user-name">{name}</h1>
+            <p id="stars" className="user-content">Star: <b>{stars}</b></p>
+            <p id="followers" className="user-content">Followers: <b>{followers}</b></p>
+        </div>
+    )
 }
 
 export default UserContent
