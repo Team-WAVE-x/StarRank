9da646b63873a876a9cc65fbb2158da4c6cee958
시혁 박 <exon.indiv@gmail.com>
2020-11-15 21:25:53 +0900

    update

--- update/update.ts
+++ update/update.ts
@@ -1,11 +1,14 @@
-const fs = require('fs')
-import getUser from '../app/src/data/GetUser.ts'
+import * as fs from 'fs'
+import getUser from '../app/src/data/GetUser'
 // const jsonPath = '../app/src/data/member.json'
 const jsonPath = '../app/src/data/member.json'
 
 fs.readFile(jsonPath, (err, data) => {
+    let parseData = JSON.parse(JSON.stringify(data))
+    console.log(parseData)
     fs.writeFile(jsonPath, data, (err) => {
-        data = JSON.parse(data)
-        console.log(data)
+        for (let i: number = 0; i < parseData.length; i++) {
+            getUser(parseData[i].name).then(d => console.log(d))
+        }
     })
 })
